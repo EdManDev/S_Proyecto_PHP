8c2d3ec78eee95671c7aeff6877ac7249d3c67dd
unknown <EdmanDev>
2019-09-05 13:11:54 -0700

    add Ui

--- index.php
+++ index.php
@@ -45,7 +45,6 @@ if ($_SERVER["REQUEST_METHOD"] == "POST") {
     $limite_de_credito = test_input($_POST["limite_de_credito"]);
   }
 
-
 // //   fecha_de_registro 
   if (empty($_POST["limite_de_credito"])) {
     $fecha_de_registroErr = "fecha_de_registro is required";
@@ -81,56 +80,69 @@ function test_input($data) {
 ?>
 <h2>PHP Form Validation Example</h2>
 <p><span class="error">(*) required field</span></p>
-<form method="post" class="formBox" action="<?php echo htmlspecialchars($_SERVER["PHP_SELF"]);?>">
-  Name:
-  <br>
-   <input type="text" name="name" value="<?php echo $name;?>">
-  <span class="error">* <?php echo $nameErr;?></span>
-  <br>
-  Estado Civil:
-  <br>
-  <input type="text" name="estado_civil" value="<?php echo $estado_civil;?>">
-  <span class="error">* <?php echo $estado_civilErr;?></span>
-  <br>
-  limite_de_credito: 
-  <br>
-  <input type="text" name="limite_de_credito" value="<?php echo $limite_de_credito;?>">
-  <span class="error">* <?php echo $limite_de_creditoErr;?></span>
-  <br>
-  Fecha_de_registro: 
-  <br>
-  <input type="text" name="fecha_de_registro" value="<?php echo $fecha_de_registro;?>">
-  <span class="error">* <?php echo $fecha_de_registroErr;?></span>
-  <br>
-  E-mail:
-  <br>
-  <input type="text" name="email" value="<?php echo $email;?>">
-  <span class="error">* <?php echo $emailErr;?></span>
-  <br>
-  Sexo:
-  <br>
-  <input type="radio" name="gender" <?php if (isset($gender) && $gender=="female") echo "checked";?> value="female">Female
-  <input type="radio" name="gender" <?php if (isset($gender) && $gender=="male") echo "checked";?> value="male">Male
-  <input type="radio" name="gender" <?php if (isset($gender) && $gender=="other") echo "checked";?> value="other">Other  
-  <span class="error">* <?php echo $genderErr;?></span>
-  <br><br>
-  <input type="submit" name="submit" value="Submit">  
-</form>
 
-<?php
-echo "<h2>Your Input:</h2>";
-echo $name;
-echo "<br>";
-echo $estado_civil;
-echo "<br>";
-echo $limite_de_credito;
-echo "<br>";
-echo $fecha_de_registro;
-echo "<br>";
-echo $email;
-echo "<br>";
-echo $gender;
-?>
+  <div class="layout">
+    <div class="service-info">
+      <div class="card card-1">
+        <form method="post" class="formBox" action="<?php echo htmlspecialchars($_SERVER["PHP_SELF"]);?>">
+          Name:
+          <br>
+          <input type="text" name="name" value="<?php echo $name;?>">
+          <span class="error">* <?php echo $nameErr;?></span>
+          <br>
+          Estado Civil:
+          <br>
+          <input type="text" name="estado_civil" value="<?php echo $estado_civil;?>">
+          <span class="error">* <?php echo $estado_civilErr;?></span>
+          <br>
+          limite_de_credito: 
+          <br>
+          <input type="text" name="limite_de_credito" value="<?php echo $limite_de_credito;?>">
+          <span class="error">* <?php echo $limite_de_creditoErr;?></span>
+          <br>
+          Fecha_de_registro: 
+          <br>
+          <input type="text" name="fecha_de_registro" value="<?php echo $fecha_de_registro;?>">
+          <span class="error">* <?php echo $fecha_de_registroErr;?></span>
+          <br>
+          E-mail:
+          <br>
+          <input type="text" name="email" value="<?php echo $email;?>">
+          <span class="error">* <?php echo $emailErr;?></span>
+          <br>
+          Sexo:
+          <br>
+          <input type="radio" name="gender" <?php if (isset($gender) && $gender=="female") echo "checked";?> value="female">Female
+          <input type="radio" name="gender" <?php if (isset($gender) && $gender=="male") echo "checked";?> value="male">Male
+          <input type="radio" name="gender" <?php if (isset($gender) && $gender=="other") echo "checked";?> value="other">Other  
+          <span class="error">* <?php echo $genderErr;?></span>
+          <br><br>
+          <input type="submit" name="submit" value="Submit">  
+        </form>
+  
+
+      </div>
+      <div class="card card-2">
+      <div class="input">
+          <?php
+          echo "<h2>Your Input Are:</h2>";
+          echo $name;
+          echo "<br>";
+          echo $estado_civil;
+          echo "<br>";
+          echo $limite_de_credito;
+          echo "<br>";
+          echo $fecha_de_registro;
+          echo "<br>";
+          echo $email;
+          echo "<br>";
+          echo $gender;
+          ?>
+  </div>
+  
+      </div>
+    </div>
+  </div>
 
 </body>
 </html>
